469f087026a49df79f5e7646556adc8c3db79060
Yuri Kuznetsov <yurikuzn@users.noreply.github.com>
2023-06-22 11:48:23 +0300

    first commit

---
+++ entry.js
@@ -0,0 +1,7 @@
+const Bundler = require('./bundler/general-bundler');
+const Transpiler = require('./transpiler/transpiler');
+const TemplateBunlder = require('./template-bundler/template-bundler');
+
+exports.Bundler = Bundler;
+exports.Transpiler = Transpiler;
+exports.TemplateBunlder = TemplateBunlder;
