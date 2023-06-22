ee38148194ff263d927152d978807414487e2d5a
Yuri Kuznetsov <yurikuzn@users.noreply.github.com>
2023-06-22 12:28:10 +0300

    fix

--- entry.js
+++ entry.js
@@ -1,7 +1,7 @@
 const Bundler = require('./bundler/general-bundler');
 const Transpiler = require('./transpiler/transpiler');
-const TemplateBunlder = require('./template-bundler/template-bundler');
+const TemplateBundler = require('./template-bundler/template-bundler');
 
 exports.Bundler = Bundler;
 exports.Transpiler = Transpiler;
-exports.TemplateBunlder = TemplateBunlder;
+exports.TemplateBundler = TemplateBundler;
